28481275e6c93363d16b15fe72a9e08b978a51d3
Laraconverso <laraconverso@gmail.com>
2023-09-11 16:10:03 -0300

    update

---
+++ ClasesPracticas/Clase6/Tenis/src/main/java/TableroTenis.java
@@ -0,0 +1,24 @@
+public class TableroTenis implements PuntajeTenis{
+    public TableroTenis() {
+       
+    }
+
+    @Override
+    public void punto(Jugador j) {
+
+    }
+    @Override
+    public Jugador servicio() {
+        return null;
+    }
+
+    @Override
+    public int[][] puntajeSets() {
+        return new int[0][];
+    }
+
+    @Override
+    public Puntos[] puntajeGame() {
+        return new Puntos[0];
+    }
+}
---
+++ ClasesPracticas/Clase6/Tenis/src/test/java/TableroTenisTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TableroTenisTest {
+
+    @Test
+    public void test1() {
+    }
+
+}
